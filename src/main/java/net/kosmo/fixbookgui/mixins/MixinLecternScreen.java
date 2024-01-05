e9e59ac26df3d172efb1fc2e37ea4e5e34587947
diskree <diskreee@gmail.com>
2024-01-05 12:04:09 +0400

    Refactoring

--- src/main/java/net/kosmo/fixbookgui/mixins/MixinLecternScreen.java
+++ src/main/java/net/kosmo/fixbookgui/mixins/MixinLecternScreen.java
@@ -1,17 +1,17 @@
 package net.kosmo.fixbookgui.mixins;
 
+import net.kosmo.fixbookgui.FixBookGui;
+import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Selectable;
 import net.minecraft.client.gui.screen.ingame.BookScreen;
 import net.minecraft.client.gui.screen.ingame.LecternScreen;
 import net.minecraft.client.gui.screen.ingame.ScreenHandlerProvider;
-import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.Widget;
 import net.minecraft.screen.LecternScreenHandler;
-import net.minecraft.screen.ScreenTexts;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * @author KosmoMoustache
@@ -19,27 +19,22 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
  */
 @Mixin(LecternScreen.class)
 public abstract class MixinLecternScreen extends BookScreen implements ScreenHandlerProvider<LecternScreenHandler> {
+
     protected MixinLecternScreen() {
         super(null);
     }
 
-    @Shadow
-    private void sendButtonPressPacket(int i) {
-    }
-
-    private int getY() {
-        return (this.height - 192) / 3 + 196;
-    }
-
-    @ModifyArg(method = "addCloseButton", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/LecternScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;", ordinal = 0))
-    public Element fbg$addCloseButton1(Element par1) {
-        return ButtonWidget.builder(ScreenTexts.DONE, (button) -> this.close())
-                .dimensions(this.width / 2 - 100, getY(), 98, 20).build();
-    }
-
-    @ModifyArg(method = "addCloseButton", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/LecternScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;", ordinal = 1))
-    public Element fbg$addCloseButton2(Element par1) {
-        return ButtonWidget.builder(Text.translatable("lectern.take_book"), (button) -> this.sendButtonPressPacket(3))
-                .dimensions(this.width / 2 + 2, getY(), 98, 20).build();
+    @Redirect(
+            method = "addCloseButton",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/screen/ingame/LecternScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;"
+            )
+    )
+    public <T extends Element & Drawable & Selectable> T fbg$translateButtons(LecternScreen screen, T element) {
+        if (element instanceof Widget widget) {
+            widget.setY(widget.getY() + FixBookGui.getFixedY(this));
+        }
+        return screen.addDrawableChild(element);
     }
 }
