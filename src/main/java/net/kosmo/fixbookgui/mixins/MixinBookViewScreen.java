017f920320e05a35fa8ebd6d631ad946ac31cf9a
KosmoMoustache <34519077+KosmoMoustache@users.noreply.github.com>
2024-06-14 21:52:57 +0200

    fix #29

--- src/main/java/net/kosmo/fixbookgui/mixins/MixinBookViewScreen.java
+++ src/main/java/net/kosmo/fixbookgui/mixins/MixinBookViewScreen.java
@@ -106,7 +106,7 @@ public abstract class MixinBookScreen extends Screen {
             method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;IIIZ)I"
+                    target = "Lnet/minecraft/client/gui/GuiGraphics;renderComponentHoverEffect(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Style;II)V"
             ),
             index = 3
     )
