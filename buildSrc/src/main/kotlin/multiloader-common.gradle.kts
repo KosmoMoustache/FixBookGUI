311dadd557c86c5eca7e9b1563a9a1d7d153c2cb
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-08-15 23:33:51 +0200

    added back parchment

--- buildSrc/src/main/kotlin/multiloader-common.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader-common.gradle.kts
@@ -1,73 +1,74 @@
 plugins {
-	id("java")
-	id("idea")
-	id("java-library")
+    id("java")
+    id("idea")
+    id("java-library")
 }
 
 version = "${loader}-${commonMod.version}+mc${stonecutterBuild.current.version}"
 
 base {
-	archivesName = commonMod.id
+    archivesName = commonMod.id
 }
 
 java {
-	toolchain.languageVersion = JavaLanguageVersion.of(commonProject.prop("java.version")!!)
-	// withSourcesJar()
-	// withJavadocJar()
+    toolchain.languageVersion = JavaLanguageVersion.of(commonProject.prop("java.version")!!)
+    // withSourcesJar()
+    // withJavadocJar()
 }
 
 repositories {
-	mavenCentral()
-	exclusiveContent {
-		forRepository {
-			maven("https://repo.spongepowered.org/repository/maven-public") { name = "Sponge" }
-		}
-		filter { includeGroupAndSubgroups("org.spongepowered") }
-	}
-	exclusiveContent {
-		forRepositories(
-			maven("https://maven.parchmentmc.org") { name = "ParchmentMC" },
-			maven("https://maven.neoforged.net/releases") { name = "NeoForge" },
-		)
-		filter { includeGroup("org.parchmentmc.data") }
-	}
-	maven("https://maven.terraformersmc.com/releases/") { name = "TerraformersMC" }
-	maven("https://maven.quiltmc.org/repository/release")
+    mavenCentral()
+    exclusiveContent {
+        forRepository {
+            maven("https://repo.spongepowered.org/repository/maven-public") { name = "Sponge" }
+        }
+        filter { includeGroupAndSubgroups("org.spongepowered") }
+    }
+    exclusiveContent {
+        forRepositories(
+            maven("https://maven.parchmentmc.org") { name = "ParchmentMC" },
+            maven("https://maven.neoforged.net/releases") { name = "NeoForge" },
+        )
+        filter { includeGroup("org.parchmentmc.data") }
+    }
+    maven("https://maven.terraformersmc.com/releases/") { name = "TerraformersMC" }
+    maven("https://maven.nucleoid.xyz") { name = "pb4.eu" }
+    maven("https://maven.quiltmc.org/repository/release")
 }
 
 tasks {
-	processResources {
-		val expandProps = mapOf(
-			"javaVersion" to commonMod.propOrNull("java.version"),
-			"modId" to commonMod.id,
-			"modName" to commonMod.name,
-			"modVersion" to commonMod.version,
-			"modGroup" to commonMod.group,
-			"modAuthor" to commonMod.author,
-			"modDescription" to commonMod.description,
-			"modLicense" to commonMod.license,
-			"modGitHub" to commonMod.github,
-			"minecraftVersion" to commonMod.propOrNull("minecraft_version"),
-			"minMinecraftVersion" to commonMod.propOrNull("min_minecraft_version"),
-			"fabricLoaderVersion" to commonMod.depOrNull("fabric-loader"),
-			"fabricApiVersion" to commonMod.depOrNull("fabric-api"),
-			"neoForgeVersion" to commonMod.depOrNull("neoforge"),
-		).filterValues { it?.isNotEmpty() == true }.mapValues { (_, v) -> v!! }
+    processResources {
+        val expandProps = mapOf(
+            "javaVersion" to commonMod.propOrNull("java.version"),
+            "modId" to commonMod.id,
+            "modName" to commonMod.name,
+            "modVersion" to commonMod.version,
+            "modGroup" to commonMod.group,
+            "modAuthor" to commonMod.author,
+            "modDescription" to commonMod.description,
+            "modLicense" to commonMod.license,
+            "modGitHub" to commonMod.github,
+            "minecraftVersion" to commonMod.propOrNull("minecraft_version"),
+            "minMinecraftVersion" to commonMod.propOrNull("min_minecraft_version"),
+            "fabricLoaderVersion" to commonMod.depOrNull("fabric-loader"),
+            "fabricApiVersion" to commonMod.depOrNull("fabric-api"),
+            "neoForgeVersion" to commonMod.depOrNull("neoforge"),
+        ).filterValues { it?.isNotEmpty() == true }.mapValues { (_, v) -> v!! }
 
-		val jsonExpandProps = expandProps.mapValues { (_, v) -> v.replace("\n", "\\\\n") }
+        val jsonExpandProps = expandProps.mapValues { (_, v) -> v.replace("\n", "\\\\n") }
 
-		filesMatching(listOf("META-INF/mods.toml", "META-INF/neoforge.mods.toml")) {
-			expand(expandProps)
-		}
+        filesMatching(listOf("META-INF/mods.toml", "META-INF/neoforge.mods.toml")) {
+            expand(expandProps)
+        }
 
-		filesMatching(listOf("pack.mcmeta", "fabric.mod.json", "*.mixins.json")) {
-			expand(jsonExpandProps)
-		}
+        filesMatching(listOf("pack.mcmeta", "fabric.mod.json", "*.mixins.json")) {
+            expand(jsonExpandProps)
+        }
 
-		inputs.properties(expandProps)
-	}
+        inputs.properties(expandProps)
+    }
 }
 
 tasks.named("processResources") {
-	dependsOn(":common:${commonMod.propOrNull("minecraft_version")}:stonecutterGenerate")
+    dependsOn(":common:${commonMod.propOrNull("minecraft_version")}:stonecutterGenerate")
 }
--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -1,55 +1,62 @@
 plugins {
-	`multiloader-loader`
-	id("net.neoforged.moddev")
-	kotlin("jvm") version "2.2.0"
-	id("com.google.devtools.ksp") version "2.2.0-2.0.2"
-	id("dev.kikugie.fletching-table.neoforge") version "0.1.0-alpha.14"
+    `multiloader-loader`
+    id("net.neoforged.moddev")
+    kotlin("jvm") version "2.2.0"
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
+    id("dev.kikugie.fletching-table.neoforge") version "0.1.0-alpha.14"
 }
 
 fletchingTable {
-	j52j.register("main") {
-		extension("json", "**/*.json5")
-	}
+    j52j.register("main") {
+        extension("json", "**/*.json5")
+    }
 }
 
 neoForge {
-	enable {
-		version = commonMod.dep("neoforge")
-	}
+    enable {
+        version = commonMod.dep("neoforge")
+    }
 }
 
 dependencies {
-	// Required dependencies
+    // Required dependencies
 //	implementation("group:${commonMod.dep("key")}")
 }
 
 neoForge {
-	accessTransformers.from(project.file("../../src/main/resources/META-INF/accesstransformer.cfg").absolutePath)
+    accessTransformers.from(project.file("../../src/main/resources/META-INF/accesstransformer.cfg").absolutePath)
 
-	runs {
-		register("client") {
-			client()
-			ideName = "NeoForge Client (${project.path})"
-		}
-	}
+    runs {
+        register("client") {
+            client()
+            ideName = "NeoForge Client (${project.path})"
+        }
+    }
 
-	mods {
-		register(commonMod.id) {
-			sourceSet(sourceSets.main.get())
-		}
-	}
+    mods {
+        register(commonMod.id) {
+            sourceSet(sourceSets.main.get())
+        }
+    }
+
+    parchment {
+        commonMod.depOrNull("parchment")?.let {
+            mappingsVersion = it
+            minecraftVersion = commonMod.mc
+        }
+    }
 }
 
 sourceSets.main {
-	resources.srcDir("src/generated/resources")
+    resources.srcDir("src/generated/resources")
 }
 
 tasks {
-	processResources {
-		exclude("${mod.id}.accesswidener")
-	}
+    processResources {
+        exclude("${mod.id}.accesswidener")
+    }
 }
 
 tasks.named("createMinecraftArtifacts") {
-	dependsOn(":neoforge:${commonMod.propOrNull("minecraft_version")}:stonecutterGenerate")
+    dependsOn(":neoforge:${commonMod.propOrNull("minecraft_version")}:stonecutterGenerate")
 }
