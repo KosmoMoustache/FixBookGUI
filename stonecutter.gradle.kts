90b63a993d04e6fed7ef7a02f1f140c1a30faa02
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-08-22 14:31:36 +0200

    bump plugins version

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("dev.kikugie.stonecutter")
 
-    id("net.neoforged.moddev") version "2.0.78" apply false
-    id("fabric-loom") version "1.10-SNAPSHOT" apply false
+    id("net.neoforged.moddev") version "2.0.97" apply false
+    id("fabric-loom") version "1.11-SNAPSHOT" apply false
 //    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
 }
 stonecutter active "1.19.4" /* [SC] DO NOT EDIT */
