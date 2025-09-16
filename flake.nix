929a7c864fdd78bee341ad97c03646e22bc8e5ac
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-09-17 00:04:24 +0200

    feat: add CI/CD for building and publishing

--- flake.nix
+++ flake.nix
@@ -6,10 +6,7 @@
   };
 
   outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    packages.x86_64-linux.default = self.packages.x86_64-linux.default;
-
-    devShells.default = nixpkgs.lib.mkShell {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
       packages = with nixpkgs.legacyPackages.x86_64-linux; [
         pkgs.gnumake
         pkgs.git
