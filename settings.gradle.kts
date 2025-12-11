3991b7a7e7e7ea0903439cda640ddaaf488cd363
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-12-11 19:51:21 +0100

    chore: update stonecutter to 0.7.11

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.7.6"
+    id("dev.kikugie.stonecutter") version "0.7.11"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
