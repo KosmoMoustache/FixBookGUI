dda4d06da561b36c578f07c828c12ff37a6fa723
KosmoMoustache <34519077+KosmoMoustache@users.noreply.github.com>
2026-06-17 02:29:33 +0200

    Feat/26.2 (#38)
    * Update to Gradle 9.5
    
    * feat: add version filtering to copy-test script
    
    * feat: add support to 26.2
    
    ---------
    
    Co-authored-by: KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.3.6" apply false
     id("net.neoforged.moddev") version "2.0.140" apply false
 }
-stonecutter active "26.1" /* [SC] DO NOT EDIT */
+stonecutter active "26.2" /* [SC] DO NOT EDIT */
 
 stonecutter parameters  {
     replacements {
