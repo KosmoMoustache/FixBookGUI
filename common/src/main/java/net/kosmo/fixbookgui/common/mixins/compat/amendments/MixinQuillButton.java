01a293bcccadff8c184763b6497f0f5a9eecf6a7
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2026-09-20 04:59:52 +0200

    fix mixins for mc 1.21.1

--- common/src/main/java/net/kosmo/fixbookgui/common/mixins/compat/amendments/MixinQuillButton.java
+++ common/src/main/java/net/kosmo/fixbookgui/common/mixins/compat/amendments/MixinQuillButton.java
@@ -19,7 +19,8 @@ public abstract class MixinQuillButton extends AbstractWidget {
 
     @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/AbstractWidget;<init>(IIIILnet/minecraft/network/chat/Component;)V",
             ordinal = 0), index = 1)
-    private static int fbg$InitSuper(int y, @Local(argsOnly = true, name = "screen") Screen screen) {
+    // Do not target by name as capture method, does not work with NeoForge
+    private static int fbg$InitSuper(int y, @Local(argsOnly = true, ordinal = 0) Screen screen) {
         return FixBookGui.getFixedY(screen) + y;
     }
 }
